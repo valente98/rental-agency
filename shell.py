81c3cd1a649d33d77fe60702213c19b9fb71441a
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-26 13:11:32 -0500

    added \n so it will skip a line after you run the function erase the press Q part from the intro function  created a function call from the core so that it will calculate the price of renting.

--- shell.py
+++ shell.py
@@ -1,7 +1,7 @@
 import core
 import disk
 def intro():
-    return input('''Welcome to Valente's 2017 Exotic Cars! Which car would you like to see?\n 
+    return input('''\nWelcome to Valente's 2017 Exotic Cars! Which car would you like to see?\n 
     Type the number of the choice of your car\n
     \t1. Alfa Romeo 4c Coupe
     \t2. Audi R8 V10 Plus
@@ -12,14 +12,14 @@ def intro():
     \t7. Dodge Viper
     \t8. Ford Mustang Shelby GT350R
     \t9. Lamborghini Huracan
-    \t10. Porche 911 GT3 RS 
-    press Q to quit\n\n''' )
+    \t10. Porche 911 GT3 RS \n''')
 
 def main():
     cars = disk.list_of_cars()
     choice = intro()
     car = core.choice_of_car(choice, cars)
     print(car)
-
+    car_1 = core.calculate_price_of_renting_with_taxes(car)
+    print(car_1)
 if __name__ == '__main__':
     main()
