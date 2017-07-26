a6e5d3fb6a04e54e8dda5478fbdb575ea37057d2
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-26 15:21:13 -0500

    did a new function that will calculate the deposit.

--- core.py
+++ core.py
@@ -1,9 +1,13 @@
+
 def choice_of_car(choice,cars):
     for item in cars:
         if item[0].startswith(choice):
-            return item
+            return item[:4]
 
 def calculate_price_of_renting_with_taxes(car):    
-       renting = car[3] * .01
-       sales_tax = renting * .07
-       return renting + sales_tax
+    renting = car[3] * .01
+    sales_tax = renting * .07
+    return renting + sales_tax
+
+def replacement(car):
+    return car[3] * .1
