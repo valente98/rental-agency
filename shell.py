405d7e6c06138333d32e375c2cd7877063e59c27
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-26 11:15:34 -0500

    created a function that will ask the user which car the user want to see. did a function call in the main function.

--- shell.py
+++ shell.py
@@ -1,7 +1,22 @@
 import core
 import disk
+def intro():
+    return input('''Welcome to Valente's 2017 Exotic Cars! Which car would you like to see?\n 
+    Type the number of the choice of your car\n
+    \t1. Alfa Romeo 4c Coupe
+    \t2. Audi R8 V10 Plus
+    \t3. BMW i8
+    \t4. Corvette z06
+    \t5. Dodge Camaro
+    \t6. Dodge Challenger
+    \t7. Dodge Viper
+    \t8. Ford Mustang Shelby GT350R
+    \t9. Lamborghini Huracan
+    \t10. Porche 911 GT3 RS 
+    press Q to quit\n\n''' )
 
 def main():
+    choice = intro()
 
 if __name__ == '__main__':
     main()
