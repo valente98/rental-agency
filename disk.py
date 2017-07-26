74a45474519dfb431888a6afd69af6141096192f
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-26 11:42:04 -0500

    added a new list to the list that for the i.d num in inventory.

--- disk.py
+++ disk.py
@@ -1,3 +1,4 @@
+import core
 def list_of_cars():
     left = []
     with open('inventory.txt', 'r') as file:
@@ -5,5 +6,5 @@ def list_of_cars():
         lines = file.readlines()
     for line in lines:
         split_string = line.strip().split(', ')
-        left.append([split_string[0], split_string[1], float(split_string[2]), float(split_string[3])])
+        left.append([split_string[0], split_string[1], split_string[2], float(split_string[3]), float(split_string[4])])
     return left
