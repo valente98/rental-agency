aad9401e3edc6eca7b63b96e7f12b94d5a857da9
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-26 10:04:49 -0500

    created a function that will make my inventory into a list of lists

---
+++ disk.py
@@ -0,0 +1,9 @@
+def list_of_cars():
+    left = []
+    with open('inventory.txt', 'r') as file:
+        file.readline()
+        lines = file.readlines()
+    for line in lines:
+        split_string = line.strip().split(', ')
+        left.append([split_string[0], split_string[1], float(split_string[2])])
+    return left
