3cb7e00fb1322e862ee2c0be2ef2ea875667284d
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-26 13:04:36 -0500

    created a function to calculate the renting of the car with the sales tax.

--- core.py
+++ core.py
@@ -2,3 +2,8 @@ def choice_of_car(choice,cars):
     for item in cars:
         if item[0].startswith(choice):
             return item
+
+def calculate_price_of_renting_with_taxes(car):    
+       renting = car[3] * .01
+       sales_tax = renting * .07
+       return renting + sales_tax
