f51728ced134f18b7160e9c999c726455ec423fc
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-27 11:32:28 -0500

    adde \n to the intro function so it would show each car in a new line. modified my main function so that it would it will check if the user is renting or returning

--- shell.py
+++ shell.py
@@ -2,17 +2,17 @@ import core
 import disk
 def intro():
     return input('''\n\tWhich car would you like to see?\n 
-    \tType the number of the choice of your car\n
-    \t1. Alfa Romeo 4c Coupe
-    \t2. Audi R8 V10 Plus
-    \t3. BMW i8
-    \t4. Corvette z06
-    \t5. Cheverlets Camaro
-    \t6. Dodge Challenger
-    \t7. Dodge Viper
-    \t8. Ford Mustang Shelby GT350R
-    \t9. Lamborghini Huracan
-    \t10. Porche 911 GT3 RS 
+    Type the number of the choice of your car\n
+    \t1. Alfa Romeo 4c Coupe\n
+    \t2. Audi R8 V10 Plus\n
+    \t3. BMW i8\n
+    \t4. Corvette z06\n
+    \t5. Cheverlets Camaro\n
+    \t6. Dodge Challenger\n
+    \t7. Dodge Viper\n
+    \t8. Ford Mustang Shelby GT350R\n
+    \t9. Lamborghini Huracan\n
+    \t10. Porche 911 GT3 RS \n
     type Q to quit the process\n''')
 def chosen_car(car, deposit, car_1):
     print('You have chosen',car,'the renting price per day is, $' + str(car_1)+'.')
@@ -24,29 +24,27 @@ def chosen_car(car, deposit, car_1):
 
 def renting_or_returning():
     print('\n\tWelcome to Valente\'s 2017 Exotic Cars!')
-    return input('''Hi! Are you returning a vehicle or renting a vehicle.
+    return input('''Hi! Are you returning a vehicle or renting a vehicle.\n
     Type in the number of your choice.\n
     \t1. I'm renting a vehicle.
     \t2. I'm returning a vehicle.\n''')
 
 def main():
     cars = disk.list_of_cars()
-    choice = intro()
-    while choice.lower() != 'q':
-        if choice == core.set_num_equal_num(choice):
-            user_choice = core.set_num_equal_num(choice)
-            car = core.choice_of_car(user_choice, cars)
-            deposit = core.replacement(car)   
-            car_1 = core.calculate_price_of_renting_with_taxes(car)
-            decision = chosen_car(car, deposit, car_1)
-            if decision == '1' or decision.lower() == 'one':
-
-            elif decision == '2' or decision.lower() == 'two':
-                choice = intro()
-            break
-        else:
-            print('Sorry invalid choice. Please try again.')
-        choice = intro()
+    choice = renting_or_returning()
+    if choice == '1' or choice.lower() == 'one':
+        picking = intro()
+        while picking.lower() != 'q':
+            if picking == core.set_num_equal_num(picking):
+                user_choice = core.set_num_equal_num(picking)
+                car = core.choice_of_car(user_choice, cars)
+                deposit = core.replacement(car)   
+                car_1 = core.calculate_price_of_renting_with_taxes(car)
+                decision = chosen_car(car, deposit, car_1)
+                break
+            else:
+                print('Sorry invalid choice. Please try again.')
+            choice = intro()
 
 if __name__ == '__main__':
     main()
