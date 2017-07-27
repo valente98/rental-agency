ced7ca79a4be9a3e612cb6df2125b76bd0b6bfd3
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-27 11:34:35 -0500

    change the perameter from choice to picking and added the methond round so that it will round the renting price to two decimal places.

--- core.py
+++ core.py
@@ -1,23 +1,23 @@
-def set_num_equal_num(choice):
-    if choice == '1' or choice.lower() == 'one':
+def set_num_equal_num(picking):
+    if picking == '1' or picking.lower() == 'one':
         return '1'
-    elif choice == '2' or choice.lower() == 'two':
+    elif picking == '2' or picking.lower() == 'two':
         return '2'
-    elif choice == '3' or choice.lower() == 'three':
+    elif picking == '3' or picking.lower() == 'three':
         return '3'
-    elif choice == '4' or choice.lower() == 'four':
+    elif picking == '4' or picking.lower() == 'four':
         return '4'
-    elif choice == '5' or choice.lower() == 'five':
+    elif picking == '5' or picking.lower() == 'five':
         return '5'
-    elif choice == '6' or choice.lower() == 'six':
+    elif picking == '6' or picking.lower() == 'six':
         return '6'
-    elif choice == '7' or choice.lower() == 'seven':
+    elif picking == '7' or picking.lower() == 'seven':
         return '7'
-    elif choice == '8' or choice.lower() == 'eight':
+    elif picking == '8' or picking.lower() == 'eight':
         return '8'
-    elif choice == '9' or choice.lower() == 'nine':
+    elif picking == '9' or picking.lower() == 'nine':
         return '9' 
-    elif choice == '10' or choice.lower() == 'ten':
+    elif picking == '10' or picking.lower() == 'ten':
         return '10'  
 
 def choice_of_car(user_choice,cars):
@@ -28,7 +28,7 @@ def choice_of_car(user_choice,cars):
 def calculate_price_of_renting_with_taxes(car):    
     renting = car[3] * .01
     sales_tax = renting * .07
-    return renting + sales_tax
+    return round((renting + sales_tax), 2)
 
 def replacement(car):
     return car[3] * .1
