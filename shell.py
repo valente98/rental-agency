5b80a5c6dc553f13bfc3964dd996da3792be577a
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-27 09:04:01 -0500

    took the input from the return and tried to set up equal to something and created a body in the function intro() so that way if the user types in the word of the number it would say error and that the user should use the numeric number instead of the word.

--- shell.py
+++ shell.py
@@ -2,8 +2,8 @@ import core
 import disk
 def intro():
     print('\n\tWelcome to Valente\'s 2017 Exotic Cars!')
-    return input('''\n\tWhich car would you like to see?\n 
-    Type the number of the choice of your car\n
+    msg = input('''\n\tWhich car would you like to see?\n 
+    \tType the number of the choice of your car\n
     \t1. Alfa Romeo 4c Coupe
     \t2. Audi R8 V10 Plus
     \t3. BMW i8
@@ -14,7 +14,10 @@ def intro():
     \t8. Ford Mustang Shelby GT350R
     \t9. Lamborghini Huracan
     \t10. Porche 911 GT3 RS \n''')
-
+    if msg.isnumeric():
+        return msg
+    else:
+        return 'Sorry in has to be in numeric form'
 def chosen_car(car, deposit, car_1):
     print('You have chosen',car,'the renting price per day is, $' + str(car_1)+'.')
     print('\nThe deposit of this vihicle will be, $'+str(deposit)+'.')
@@ -25,12 +28,10 @@ def chosen_car(car, deposit, car_1):
 
 def main():
     cars = disk.list_of_cars()
-    
     choice = intro()
     car = core.choice_of_car(choice, cars)
     deposit = core.replacement(car)   
     car_1 = core.calculate_price_of_renting_with_taxes(car)
-    
     decision = chosen_car(car, deposit, car_1)
 if __name__ == '__main__':
     main()
