5dd5df0dbae1ed88187264c8119af8f0f68ae108
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-27 15:22:17 -0500

    changed the choice to picking

--- core.py
+++ core.py
@@ -1,8 +1,8 @@
 def set_num_equal_num(picking):
     d = {'one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9','ten':'10'}
     if picking in d.keys():
-        choice = d[picking]
-    return choice
+        picking = d[picking]
+    return picking
 def choice_of_car(user_choice,cars):
     for item in cars:
         if item[0].startswith(user_choice):
