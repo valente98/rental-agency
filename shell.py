871567474235202b2c40597093c8b846ea52984e
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-27 09:21:03 -0500

    changed the input back into the return and deleted the body because it didn't work when the user type in the word of the num.

--- shell.py
+++ shell.py
@@ -2,7 +2,7 @@ import core
 import disk
 def intro():
     print('\n\tWelcome to Valente\'s 2017 Exotic Cars!')
-    msg = input('''\n\tWhich car would you like to see?\n 
+    return input('''\n\tWhich car would you like to see?\n 
     \tType the number of the choice of your car\n
     \t1. Alfa Romeo 4c Coupe
     \t2. Audi R8 V10 Plus
@@ -14,10 +14,6 @@ def intro():
     \t8. Ford Mustang Shelby GT350R
     \t9. Lamborghini Huracan
     \t10. Porche 911 GT3 RS \n''')
-    if msg.isnumeric():
-        return msg
-    else:
-        return 'Sorry in has to be in numeric form'
 def chosen_car(car, deposit, car_1):
     print('You have chosen',car,'the renting price per day is, $' + str(car_1)+'.')
     print('\nThe deposit of this vihicle will be, $'+str(deposit)+'.')
