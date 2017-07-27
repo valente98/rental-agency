2699c8ef12d54cd4d7db5eb72580a7e6b8a4bda8
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-27 11:04:19 -0500

    created a function that will check if what the user types in is an actual answer or not. change the perameters in the choice_of_car function.

--- core.py
+++ core.py
@@ -1,7 +1,28 @@
+def set_num_equal_num(choice):
+    if choice == '1' or choice.lower() == 'one':
+        return '1'
+    elif choice == '2' or choice.lower() == 'two':
+        return '2'
+    elif choice == '3' or choice.lower() == 'three':
+        return '3'
+    elif choice == '4' or choice.lower() == 'four':
+        return '4'
+    elif choice == '5' or choice.lower() == 'five':
+        return '5'
+    elif choice == '6' or choice.lower() == 'six':
+        return '6'
+    elif choice == '7' or choice.lower() == 'seven':
+        return '7'
+    elif choice == '8' or choice.lower() == 'eight':
+        return '8'
+    elif choice == '9' or choice.lower() == 'nine':
+        return '9' 
+    elif choice == '10' or choice.lower() == 'ten':
+        return '10'  
 
-def choice_of_car(choice,cars):
+def choice_of_car(user_choice,cars):
     for item in cars:
-        if item[0].startswith(choice):
+        if item[0].startswith(user_choice):
             return item[:4]
 
 def calculate_price_of_renting_with_taxes(car):    
