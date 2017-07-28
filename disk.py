d97ec4fb7a7c127227303356dd18c493903e0661
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-28 11:29:52 -0500

    created a function that will take the amount the user wants to rent from the amount in the inventory.

--- disk.py
+++ disk.py
@@ -8,3 +8,21 @@ def list_of_cars():
         split_string = line.strip().split(', ')
         left.append([split_string[0], split_string[1], split_string[2], float(split_string[3]), float(split_string[4])])
     return left
+
+def num_of_car(amount,car):
+    str_l = ['i.d num, name_of_car, price, amount_of_car']
+    left = list_of_cars()
+    for item in left:
+        if item[0] == car:
+            if int(amount) > int(item[4]):
+                return False
+            else:
+                item[4] = int(item[4]) - int(amount)
+        item[3] = str(item[3])
+        item[4] = str(item[4])
+        str_l.append(', '.join(item))
+        message = '\n'.join(str_l)
+
+    with open('inventory.txt', 'w') as file: 
+        file.write(message)
+    return True
