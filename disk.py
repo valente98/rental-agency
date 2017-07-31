46b106a7b922ddae7bae717422a529cca476297f
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-31 10:03:23 -0500

    added function that will help the program keep history of what each costumer rented. but is not finish yet.

--- disk.py
+++ disk.py
@@ -26,3 +26,18 @@ def num_of_car(amount,car):
     with open('inventory.txt', 'w') as file: 
         file.write(message)
     return True
+
+# def help_keep_history():
+#     message = '\n{}, {}, ${}'.format(get_gas_type, amount, price)
+#     with open('log.txt', 'a') as file:
+#         file.write(message)
+
+def in_the_log():
+    left = []
+    with open('history.txt', 'r') as file:
+        file.readline()
+        lines = file.readlines()
+    for line in lines:
+        split_string = line.strip().split(', ')
+        left.append([split_string[0], float(split_string[1]), float(split_string[2].strip().replace('$', ''))])
+    return left
