73bf9108e79dc28ac3c8cb4d5162b78f29a82af4
valente alvarez <valvarez@basecampcodingacademy.org>
2017-08-01 09:02:00 -0500

    changed one of the perameters from amount to decision.

--- disk.py
+++ disk.py
@@ -9,15 +9,15 @@ def list_of_cars():
         left.append([split_string[0], split_string[1], split_string[2], float(split_string[3]), float(split_string[4])])
     return left
 
-def num_of_car(amount,car):
+def num_of_car(decision,car):
     str_l = ['i.d num, name_of_car, price, amount_of_car']
     left = list_of_cars()
     for item in left:
         if item == car:
-            if int(amount) > int(item[4]):
+            if int(decision) > int(item[4]):
                 return False
             else:
-                item[4] = int(item[4]) - int(amount)
+                item[4] = int(item[4]) - int(decision)
         item[3] = str(item[3])
         item[4] = str(item[4])
         str_l.append(', '.join(item))
@@ -27,7 +27,7 @@ def num_of_car(amount,car):
         file.write(message)
     return True
 
-def help_keep_history(total, car, amount):
-    message = '\n{}, ${}, {}'.format(car[:3], total, amount)
+def help_keep_history(total, car, decision):
+    message = '\n{}, ${}, {}'.format(car[:3], total, decision)
     with open('history.txt', 'a') as file:
         file.write(message)
