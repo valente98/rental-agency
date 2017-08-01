e2fb5ef8eb2c2a895012efaaead78462f1f2194b
valente alvarez <valvarez@basecampcodingacademy.org>
2017-08-01 11:43:38 -0500

    creat functions that helped test my core.

--- test_core.py
+++ test_core.py
@@ -5,3 +5,23 @@ def test_choice_of_car():
         ['b','b','c',123.3]
         ]
     assert core.choice_of_car('a',l) == ['a','b','c',123.5]
+
+def test_set_num_equal_num():
+    d = {'one':'1','two':'2','three':'3'}
+    assert core.set_num_equal_num('three') == '3'
+    assert core.set_num_equal_num('one') == '1'
+
+def test_calculate_price_of_renting_with_taxes():
+    car=['a','b','c',12]
+    assert core.calculate_price_of_renting_with_taxes(car) ==.13
+
+def test_replacement():
+    car=['a','b','c',15]
+    assert core.replacement(car) == 1.5
+
+def test_calculate_total_price():
+    assert core.calculate_total_price(50, 100, 1, 3) == 450
+
+def test_calc_return_depository():
+    car=['a','b','c',25]
+    assert core.calc_return_depository(car, 3) == 7.5
