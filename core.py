9d5b9e67d31457e5a9580b5fa7dd7b7cc19860f6
valente alvarez <valvarez@basecampcodingacademy.org>
2017-08-08 09:20:34 -0500

    did control s it autocorrects my function or makes it shorter.

--- core.py
+++ core.py
@@ -9,11 +9,24 @@ def set_num_equal_num(picking):
     >>> set_num_equal_num('three')
     '3'
     '''
-    d = {'one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9','ten':'10'}
+    d = {
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9',
+        'ten': '10'
+    }
     if picking in d.keys():
         picking = d[picking]
     return picking
-def choice_of_car(user_choice,cars):
+
+
+def choice_of_car(user_choice, cars):
     '''(string, list) -> list
     takes in a list and a string and returns the list that starts with the string
     >>> choice_of_car('1', [['1', 'b', 'c']])
@@ -23,15 +36,17 @@ def choice_of_car(user_choice,cars):
         if item[0].startswith(user_choice):
             return item
 
+
 def calculate_price_of_renting_with_taxes(car):
     '''list -> float
     takes a list and return the float of the total price
     >>> calculate_price_of_renting_with_taxes(['a', 'b', 'c', 20])
     0.21
-    '''    
+    '''
     renting = car[3] * .01
     sales_tax = renting * .07
     return round((renting + sales_tax), 2)
+
 
 def replacement(car):
     '''list -> float
@@ -43,6 +58,7 @@ def replacement(car):
     '''
     return car[3] * .1
 
+
 def calculate_total_price(car_1, deposit, decision, days):
     '''(float, float, int, int) -> float
 
@@ -52,6 +68,7 @@ def calculate_total_price(car_1, deposit, decision, days):
     49.32
     '''
     return round((((car_1 + deposit) * int(decision)) * int(days)), 2)
+
 
 def calc_return_depository(car, amount):
     '''(list, float) -> float
