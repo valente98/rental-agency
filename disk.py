a53116540d5b8f903468995d12f27a82cb6f4524
valente alvarez <valvarez@basecampcodingacademy.org>
2017-08-08 09:21:41 -0500

    did control s it autocorrects my function or makes it shorter and added a new perameter to a function so that it would save the user name in the history file.

--- disk.py
+++ disk.py
@@ -1,4 +1,6 @@
 import core
+
+
 def list_of_cars():
     left = []
     with open('inventory.txt', 'r') as file:
@@ -6,10 +8,15 @@ def list_of_cars():
         lines = file.readlines()
     for line in lines:
         split_string = line.strip().split(', ')
-        left.append([split_string[0], split_string[1], split_string[2], float(split_string[3]), float(split_string[4])])
+        left.append([
+            split_string[0], split_string[1], split_string[2],
+            float(split_string[3]),
+            float(split_string[4])
+        ])
     return left
 
-def num_of_car(decision,car):
+
+def num_of_car(decision, car):
     str_l = ['i.d num, name_of_car, price, amount_of_car']
     left = list_of_cars()
     for item in left:
@@ -23,22 +30,24 @@ def num_of_car(decision,car):
         str_l.append(', '.join(item))
         message = '\n'.join(str_l)
 
-    with open('inventory.txt', 'w') as file: 
+    with open('inventory.txt', 'w') as file:
         file.write(message)
     return True
 
-def help_keep_history(total, car, decision):
-    message = '\n{}, ${}, {}'.format(car[:3], total, decision)
+
+def help_keep_history(user_name, total, car, decision):
+    message = '\n{}, ${}, {}'.format(user_name, car[:3], total, decision)
     with open('history.txt', 'a') as file:
         file.write(message)
+
 
 def return_inventory(amount, cars, car):
     str_l = ['i.d num, name_of_car, price, amount_of_car']
     for item in cars:
         if item == car:
             item[4] += int(amount)
-        item[3]=str(item[3])
-        item[4]= str(item[4])
+        item[3] = str(item[3])
+        item[4] = str(item[4])
         str_l.append(', '.join(item))
         message = '\n'.join(str_l)
 
