db9ce25f872f0aa997c013a54fb5639b0e04b172
valente alvarez <valvarez@basecampcodingacademy.org>
2017-08-08 11:18:22 -0500

    created two functions that will let the history update everytime the user returns a car.

--- disk.py
+++ disk.py
@@ -16,6 +16,21 @@ def list_of_cars():
     return left
 
 
+def history():
+    left = []
+    with open('history.txt', 'r') as file:
+        file.readline()
+        lines = file.readlines()
+    for line in lines:
+        split_string = line.strip().split(', ')
+        left.append([
+            split_string[0], split_string[1], split_string[2], split_string[3],
+            float(split_string[4]),
+            int(split_string[5])
+        ])
+    return left
+
+
 def num_of_car(decision, car):
     str_l = ['i.d num, name_of_car, price, amount_of_car']
     left = list_of_cars()
@@ -36,8 +51,24 @@ def num_of_car(decision, car):
 
 
 def help_keep_history(user_name, total, car, decision):
-    message = '\n{}, ${}, {}'.format(user_name, car[:3], total, decision)
+    message = '\n{}, {}, {}, {}'.format(user_name, car[:3], total, decision)
     with open('history.txt', 'a') as file:
+        file.write(message)
+
+
+def update_history(user_name, amount, deposit):
+    str_l = ['name, i.d num and name_of_car, price, amount_of_car']
+    histories = history()
+    for item in histories:
+        if item[0] == user_name.lower():
+            item[4] = round(item[4] - (float(deposit) * float(amount)), 2)
+            item[5] = item[5] - int(amount)
+        item[4] = str(item[4])
+        item[5] = str(item[5])
+        str_l.append(', '.join(item))
+        message = '\n'.join(str_l)
+
+    with open('history.txt', 'w') as file:
         file.write(message)
 
 
